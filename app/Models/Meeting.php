e6af0ea37d648937b0cbe841b27241bb9366ce10
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-06-17 09:48:54 +0800

    first commit laravel

---
+++ app/Models/Meeting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Meeting extends Model
+{
+    protected $fillable = [
+        'name',
+        'course',
+        'date',
+    ];
+}
