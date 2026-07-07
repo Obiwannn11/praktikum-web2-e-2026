16899af039df9a0b68023c0bf11cd513f85771ef
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-07-08 01:46:10 +0800

    feat(request): tambahkan RegisterRequest

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nama Wajib Diisi',
+            'email.required' => 'Email Wajib Diisi',
+            'email.email' => 'Format Email Tidak Valid',
+            'email.unique' => 'Email Sudah Terdaftar',
+            'password.required' => 'Password Wajib Diisi',
+            'password.min' => 'Password Minimal :min Karakter',
+            'password.confirmed' => 'Konfirmasi Password Tidak Cocok',
+        ];
+    }
+}
