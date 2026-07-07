16cca29ea7a23d9f9a717ff2fad057e0692c59d4
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-07-08 01:44:41 +0800

    feat(route): daftarkan resource route categories books dan borrows

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
+Route::apiResource('borrows', BorrowController::class);
