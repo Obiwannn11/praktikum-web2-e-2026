b2a1f0255713a133fd070b05c65ad93da8a0f02f
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-07-08 01:46:10 +0800

    feat(model): tambahkan HasApiTokens pada model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,24 +4,26 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
-        'password',
         'email',
+        'password',
         'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -37,7 +39,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function borrow(){
-        return $this->belongsTo(Borrow::class);
+    public function borrows()
+    {
+        return $this->hasMany(Borrow::class);
     }
 }
